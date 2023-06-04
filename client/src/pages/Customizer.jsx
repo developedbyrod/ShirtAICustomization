ba8b758cc073b4c861dbd0e018bfb19e61a1ac30
Rodolfo Augusto <rar2023@icloud.com>
2023-06-04 20:53:40 -0300

    init

---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,20 @@
+
+import { useState, useEffect } from "react";
+import {AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import config from "../config/config";
+import state from "../store";
+
+import { download } from "../assets";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+
+const Customizer = () => {
+  return (
+    <div>Customizer</div>
+  )
+}
+
+export default Customizer
